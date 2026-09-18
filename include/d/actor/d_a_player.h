026064d98d710342a11dbb0d0dfa7d82f273cbb1
kamilink7 <ethanthomas1309@outlook.com>
2026-09-17 21:49:15 -0700

    Fixed d_a_alink.h offset alignment

--- include/d/actor/d_a_player.h
+++ include/d/actor/d_a_player.h
@@ -93,14 +93,6 @@ public:
 #define PLAYER_CREATE_ANM_HEAP(heap, type, name) (heap).createHeap(type)
 #endif
 
-#if TARGET_PC
-inline u32 daPy_getAnmResourceSize(u16 i_resId, u32 i_minSize) {
-    JKRArchive* archive = dComIfGp_getAnmArchive();
-    u32 size = archive->getFileSize(archive->findIdxResource(i_resId));
-    return size > i_minSize ? size : i_minSize;
-}
-#endif
-
 class daPy_anmHeap_c {
 public:
     enum daAlinkHEAP_TYPE {
@@ -117,9 +109,6 @@ public:
     void* mallocBuffer();
 #if TARGET_PC
     void createHeap(daAlinkHEAP_TYPE i_heapType, const char* name);
-    void reserveBuffer(u16 i_resId);
-    void* allocTempBuffer(u16 i_resId, u32* io_size);
-    void freeTempBuffers();
 #else
     void createHeap(daAlinkHEAP_TYPE i_heapType);
 #endif
@@ -149,10 +138,6 @@ public:
     /* 0x08 */ u32 mBufferSize;
     /* 0x0C */ u8* mBuffer;
     /* 0x10 */ JKRSolidHeap* mAnimeHeap;
-#if TARGET_PC
-    u8* mOwnedBuffer = NULL;
-    void** mTempBuffers = NULL;
-#endif
 };  // Size = 0x14
 
 class daPy_actorKeep_c {
--- src/d/actor/d_a_player.cpp
+++ src/d/actor/d_a_player.cpp
@@ -228,12 +228,6 @@ daPy_anmHeap_c::~daPy_anmHeap_c() {
     if (mAnimeHeap != NULL) {
         mDoExt_destroySolidHeap(mAnimeHeap);
     }
-#if TARGET_PC
-    freeTempBuffers();
-    if (mOwnedBuffer != NULL) {
-        JKRFreeToSysHeap(mOwnedBuffer);
-    }
-#endif
 }
 
 void daPy_anmHeap_c::initData() {
@@ -246,56 +240,6 @@ void* daPy_anmHeap_c::mallocBuffer() {
     mBuffer = JKR_NEW_ARRAY_ARGS(u8, mBufferSize, 0x20);
     return mBuffer;
 }
-
-#if TARGET_PC
-constexpr u32 kAlignment = 0x20;
-
-void daPy_anmHeap_c::reserveBuffer(u16 i_resId) {
-    // Ensure mBuffer is large enough to hold the resource
-    u32 size = daPy_getAnmResourceSize(i_resId, mBufferSize);
-    if (size <= mBufferSize) {
-        return;
-    }
-
-    // If not, replace it with a new buffer allocated from the system heap. Callers still copy
-    // archive data in on every load: setAnmTransform bswaps key tables in place, so we can't point
-    // it at the archive's cached copy directly.
-    u8* buffer = static_cast<u8*>(JKRAllocFromSysHeap(size, kAlignment));
-    JUT_ASSERT(__LINE__, buffer != NULL);
-    if (mOwnedBuffer != NULL) {
-        JKRFreeToSysHeap(mOwnedBuffer);
-    }
-    mOwnedBuffer = buffer;  // Mark it as owned so we release it later
-    mBuffer = buffer;
-    mBufferSize = size;
-}
-
-void* daPy_anmHeap_c::allocTempBuffer(u16 i_resId, u32* io_size) {
-    // Check if the resource can fit in io_size
-    u32 size = daPy_getAnmResourceSize(i_resId, *io_size);
-    if (size <= *io_size) {
-        return JKR_NEW_ARRAY_ARGS(u8, *io_size, kAlignment);
-    }
-
-    // If not, allocate a new temp buffer from the system heap, plus kAlignment extra bytes.
-    // We stash a pointer to the next buffer at the beginning, forming a chain so we can free
-    // them all later.
-    void** temp = static_cast<void**>(JKRAllocFromSysHeap(kAlignment + size, kAlignment));
-    JUT_ASSERT(__LINE__, temp != NULL);
-    *temp = mTempBuffers;
-    mTempBuffers = temp;
-    *io_size = size;
-    return reinterpret_cast<u8*>(temp) + kAlignment;
-}
-
-void daPy_anmHeap_c::freeTempBuffers() {
-    while (mTempBuffers != NULL) {
-        void** temp = mTempBuffers;
-        mTempBuffers = static_cast<void**>(*temp);
-        JKRFreeToSysHeap(temp);
-    }
-}
-#endif
 
 void daPy_anmHeap_c::createHeap(daPy_anmHeap_c::daAlinkHEAP_TYPE i_heapType, const char* name) {
     u32 size;
@@ -370,7 +314,6 @@ void* daPy_anmHeap_c::loadData(u16 i_resId) {
     };
 
     if (mArcNo == 0xFFFF) {
-        IF_DUSK(reserveBuffer(i_resId);)
         JKRReadIdxResource(mBuffer, mBufferSize, i_resId, dComIfGp_getAnmArchive());
         #if DEBUG
         daPy_aramBufferCheck(mBuffer, mBufferSize);
@@ -424,7 +367,6 @@ void* daPy_anmHeap_c::loadDataDemoRID(u16 i_resID, u16 i_arcNo) {
 
 JKRHeap* daPy_anmHeap_c::setAnimeHeap() {
     mAnimeHeap->freeAll();
-    IF_DUSK(freeTempBuffers();)
     return mDoExt_setCurrentHeap(mAnimeHeap);
 }
 
